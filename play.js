780f9847b528d1e37f0253211a7a0b3937e1c035
Robby Dhindsa <dhindsa.1996@gmail.com>
2023-01-18 16:58:05 -0800

    stdin setup

--- play.js
+++ play.js
@@ -3,3 +3,25 @@ const connect = require('./client');
 
 console.log("Connecting ...");
 connect();
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  const handleUserInput = function (data) {
+    // console.log(data);
+    // \u0003 maps to ctrl+c input
+    if (data === '\u0003') {
+      process.exit();
+    }
+  };
+  
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+setupInput();
