17e3f4ea410b359f3bd4bbcffef0b2cac4507ca5
Robby Dhindsa <dhindsa.1996@gmail.com>
2023-01-18 18:36:27 -0800

    Added WASD movement

--- input.js
+++ input.js
@@ -1,5 +1,9 @@
+// Stores the active TCP connection object.
+let connection;
+
 // setup interface to handle user input from stdin
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -10,6 +14,18 @@ const setupInput = function () {
     // \u0003 maps to ctrl+c input
     if (data === '\u0003') {
       process.exit();
+    }
+    if (data === 'w') {
+      conn.write("Move: up");
+    }
+    if (data === 'a') {
+      conn.write("Move: left");
+    }
+    if (data === 's') {
+      conn.write("Move: down");
+    }
+    if (data === 'd') {
+      conn.write("Move: right");
     }
   };
   
